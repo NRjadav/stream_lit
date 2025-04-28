80edc06214c7e095e30ae9347ccf9d7bbe65d6ab
NRjadav <109718224+NRjadav@users.noreply.github.com>
2025-04-28 15:44:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-# app.py
 import streamlit as st
 
+# Hide Streamlit Main Menu, Footer, and Header
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Your App Content
 st.title('Hello Streamlit!')
 st.write('Welcome to your first Streamlit app.')
 
