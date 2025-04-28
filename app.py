4ef0a4627099dcec8e907067fdab668367d4b25c
NRjadav <109718224+NRjadav@users.noreply.github.com>
2025-04-28 15:40:33 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 
-st.title('Hello Streamlit!')
+st.title('Hello Streamlitok!')
 st.write('Welcome to your first Streamlit app.')
 
 name = st.text_input('Enter your name:')
